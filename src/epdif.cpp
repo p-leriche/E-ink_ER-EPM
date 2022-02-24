e13471231384df19096f3627cc019493dc206bde
JustBobinAround <67753581+JustBobinAround@users.noreply.github.com>
2022-02-24 09:43:50 -0500

    Create epdif.cpp
    Creating source folder

---
+++ src/epdif.cpp
@@ -0,0 +1,41 @@
+/***************************************************
+//Web: http://www.buydisplay.com
+EastRising Technology Co.,LTD
+****************************************************/
+
+#include "epdif.h"
+#include <spi.h>
+
+EpdIf::EpdIf() {
+};
+
+EpdIf::~EpdIf() {
+};
+
+void EpdIf::DigitalWrite(int pin, int value) {
+    digitalWrite(pin, value);
+}
+
+int EpdIf::DigitalRead(int pin) {
+    return digitalRead(pin);
+}
+
+void EpdIf::DelayMs(unsigned int delaytime) {
+    delay(delaytime);
+}
+
+void EpdIf::SpiTransfer(unsigned char data) {
+    digitalWrite(cs_pin, LOW);
+    SPI.transfer(data);
+    digitalWrite(cs_pin, HIGH);
+}
+
+int EpdIf::IfInit(void) {
+    pinMode(cs_pin, OUTPUT);
+    pinMode(reset_pin, OUTPUT);
+    pinMode(dc_pin, OUTPUT);
+    pinMode(busy_pin, INPUT); 
+    SPI.beginTransaction(SPISettings(2000000, MSBFIRST, SPI_MODE0));
+    SPI.begin();
+    return 0;
+}
