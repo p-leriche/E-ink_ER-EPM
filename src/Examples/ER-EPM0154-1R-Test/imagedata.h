1d94af7403cc46ae299b496b5bd7aad10e128f65
JustBobinAround <67753581+JustBobinAround@users.noreply.github.com>
2022-02-24 09:49:08 -0500

    Add files via upload
    Adding example source

---
+++ src/Examples/ER-EPM0154-1R-Test/imagedata.h
@@ -0,0 +1,13 @@
+/***************************************************
+//Web: http://www.buydisplay.com
+EastRising Technology Co.,LTD
+****************************************************/
+
+extern const unsigned char IMAGE_BLACK[];
+extern const unsigned char IMAGE_BLACK1[];
+extern const unsigned char IMAGE_RED[];
+extern const unsigned char IMAGE_RED1[];
+
+/* FILE END */
+
+
